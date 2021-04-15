c29c0d4a2c9eb711f6e4d17a8d066e5a16d74849
Teodoro Arioli <teodoro.arioli@mail.polimi.it>
2021-04-16 00:29:25 +0200

    updated pom updated test classes

---
+++ src/test/java/LeaderCardCollectorTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaderCardCollectorTest {
+
+    @Test
+    void discountConstructor() {
+    }
+
+    @Test
+    void extraDepotConstructor() {
+    }
+
+    @Test
+    void whiteConverterConstructor() {
+    }
+
+    @Test
+    void extraProdConstructor() {
+    }
+
+    @Test
+    void build() {
+    }
+}
