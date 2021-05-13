6a460d5e5577698b6214bb97518e45d5c1b87d18
Teodoro Arioli <teodoro.arioli@mail.polimi.it>
2021-05-13 17:24:32 +0200

    updated notifymessage

--- src/main/java/network/messages/gameMessages/BuyDevCardResponse.java
+++ src/main/java/network/messages/gameMessages/BuyDevCardResponse.java
@@ -3,6 +3,7 @@ package network.messages.gameMessages;
 import network.messages.MessageType;
 
 public class BuyDevCardResponse extends GameMessage{
+
     public BuyDevCardResponse(String username) {
         super(username, MessageType.MARKETUPDATE);
     }
--- src/main/java/network/messages/notifies/NotifyMessage.java
+++ src/main/java/network/messages/notifies/NotifyMessage.java
@@ -4,11 +4,14 @@ import network.messages.Message;
 import network.messages.MessageType;
 
 public abstract class NotifyMessage extends Message {
-    private String username;
     private String message;
 
     public NotifyMessage(String username, MessageType messageType, String message){
         super(username, messageType);
         this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
     }
 }
