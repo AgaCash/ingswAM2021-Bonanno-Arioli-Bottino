8bd78f67f6b50a90f3877f0e78ba2b85d789e5c2
Teodoro Arioli <teodoro.arioli@mail.polimi.it>
2021-07-02 18:39:38 +0200

    fixing colours

--- src/main/java/clientModel/resources/LightResource.java
+++ src/main/java/clientModel/resources/LightResource.java
@@ -1,5 +1,7 @@
 package clientModel.resources;
 
+import clientModel.colour.LightColour;
+
 /**
  * LightModel copy of Model's Resource
  */
@@ -15,11 +17,11 @@ public enum LightResource {
      */
     public String toColoredString(){
         switch(this){
-            case COIN: return "\u001B[33m"+"CO"+"\u001B[0m";
-            case FAITH: return "\u001B[31m"+"FA" +"\u001B[0m";
-            case SERVANT: return "\u001B[35m" +"SE" +"\u001B[0m";
-            case SHIELD: return "\u001B[34m" + "SH" + "\u001B[0m";
-            case STONE: return "\u001B[30m"+"ST"+ "\u001B[0m";
+            case COIN: return LightColour.YELLOW +"CO"+LightColour.WHITE;
+            case FAITH: return LightColour.RED+"FA" +LightColour.WHITE;
+            case SERVANT: return LightColour.PURPLE +"SE" +LightColour.WHITE;
+            case SHIELD: return LightColour.BLUE + "SH" + LightColour.WHITE;
+            case STONE: return LightColour.BLACK+"ST"+ LightColour.WHITE;
         }
         return null;
     }
